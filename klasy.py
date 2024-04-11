f4b8ee09ca61f98f1a31140de6227b2870f3943d
Prz <przemek.piotrowski@gmail.com>
2024-04-11 14:25:02 +0200

    add wyj2

---
+++ klasy.py
@@ -0,0 +1,29 @@
+class Human:
+    """
+    Klasa Human opisująca człowieka w python
+    """
+    imie = ""
+    wiek = None
+    plec= "k"
+
+
+# print(Human.__doc__)
+# print(print.__doc__)
+
+cz1 = Human()
+cz1.imie="Anna"
+cz1.wiek = 56
+
+print(cz1)
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+
+cz2 = Human()
+cz2.imie="Adam"
+cz2.wiek = 46
+cz2.plec="m"
+print(cz2)
+print(cz2.imie)
+print(cz2.wiek)
+print(cz2.plec)
