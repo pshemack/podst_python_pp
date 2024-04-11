b1657af9b92851a5a1202712e4c742062d8638bd
Prz <przemek.piotrowski@gmail.com>
2024-04-11 09:26:03 +0200

    add wyj2

--- wyj_zad2.py
+++ wyj_zad2.py
@@ -1,13 +1,19 @@
-try:
-    z1=float(input("Zmienna1:"))
-    d=input("Działanie(/*):")
-    z2=float(input("Zmienna2:"))
+# while True
+#
+# try:
+#     z1=float(input("Zmienna1:"))
+#     d=input("Działanie(/*):")
+#     z2=float(input("Zmienna2:"))
+#
+#     match d:
+#         case "*":
+#             print(z1*z2)
+#         case "/":
+#             print(z1/z2)
+#
+# except ZeroDivisionError as e:
+#     print("Nie dziel przez zero",e)
 
-    match d:
-        case "*":
-            print(z1*z2)
-        case "/":
-            print(z1/z2)
-
-except ZeroDivisionError as e:
-    print("Nie dziel przez zero",e)
+while True:
+    odp = input("Podaj wyrażenie")
+    print(eval(odp))
