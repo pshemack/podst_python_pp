effbb32a11a0f2abfa7e817e5397ee9a19ddf39f
Prz <przemek.piotrowski@gmail.com>
2024-04-11 09:25:21 +0200

    add petle zad3

---
+++ wyj_zad2.py
@@ -0,0 +1,13 @@
+try:
+    z1=float(input("Zmienna1:"))
+    d=input("Działanie(/*):")
+    z2=float(input("Zmienna2:"))
+
+    match d:
+        case "*":
+            print(z1*z2)
+        case "/":
+            print(z1/z2)
+
+except ZeroDivisionError as e:
+    print("Nie dziel przez zero",e)
